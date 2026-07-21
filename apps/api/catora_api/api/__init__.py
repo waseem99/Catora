fcc61e4e112f86aa3f36a39448d953a927e37ae8
waseem99 <waseem99@users.noreply.github.com>
2026-07-21 15:17:53 +0500

    Export catalog ingestion API router

--- apps/api/catora_api/api/__init__.py
+++ apps/api/catora_api/api/__init__.py
@@ -1,3 +1,4 @@
 from catora_api.api.auth import router as auth_router
+from catora_api.api.ingestion import router as ingestion_router
 
-__all__ = ["auth_router"]
+__all__ = ["auth_router", "ingestion_router"]
