04a30eb5469a579952370dc6379e6458ceed8b10
waseem99 <waseem99@users.noreply.github.com>
2026-07-21 15:59:15 +0500

    Export Shopify source API router

--- apps/api/catora_api/api/__init__.py
+++ apps/api/catora_api/api/__init__.py
@@ -1,4 +1,5 @@
 from catora_api.api.auth import router as auth_router
 from catora_api.api.ingestion import router as ingestion_router
+from catora_api.api.shopify import router as shopify_router
 
-__all__ = ["auth_router", "ingestion_router"]
+__all__ = ["auth_router", "ingestion_router", "shopify_router"]
