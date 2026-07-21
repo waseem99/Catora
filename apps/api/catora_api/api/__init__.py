8603113ad6c9ecfb57ee04e6be4eef34931052e1
waseem99 <waseem99@users.noreply.github.com>
2026-07-21 16:18:19 +0500

    Export public catalog API router

--- apps/api/catora_api/api/__init__.py
+++ apps/api/catora_api/api/__init__.py
@@ -1,5 +1,11 @@
 from catora_api.api.auth import router as auth_router
 from catora_api.api.ingestion import router as ingestion_router
+from catora_api.api.public_catalog import router as public_catalog_router
 from catora_api.api.shopify import router as shopify_router
 
-__all__ = ["auth_router", "ingestion_router", "shopify_router"]
+__all__ = [
+    "auth_router",
+    "ingestion_router",
+    "public_catalog_router",
+    "shopify_router",
+]
