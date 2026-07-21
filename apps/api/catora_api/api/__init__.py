28e2349fa6179cf6e72fdcf5c6b3b48dc0cd64e8
waseem99 <waseem99@users.noreply.github.com>
2026-07-21 21:53:11 +0500

    Export taxonomy API router

--- apps/api/catora_api/api/__init__.py
+++ apps/api/catora_api/api/__init__.py
@@ -4,6 +4,7 @@ from catora_api.api.catalog_identity import router as catalog_identity_router
 from catora_api.api.ingestion import router as ingestion_router
 from catora_api.api.public_catalog import router as public_catalog_router
 from catora_api.api.shopify import router as shopify_router
+from catora_api.api.taxonomy import router as taxonomy_router
 
 __all__ = [
     "auth_router",
@@ -12,4 +13,5 @@ __all__ = [
     "ingestion_router",
     "public_catalog_router",
     "shopify_router",
+    "taxonomy_router",
 ]
